6fa7f69dcba006969321c55e84daa316c0abefca
Ignacio Mazuecos <mazuecos18@gmail.com>
2016-02-17 18:16:09 +0100

    Version 1602171816

--- app/src/main/java/dam/aventuradebolsillo/EquipoActivity.java
+++ app/src/main/java/dam/aventuradebolsillo/EquipoActivity.java
@@ -8,5 +8,7 @@ public class EquipoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipo);
+
+
     }
 }
