0e0bd6a371589af3fe7f0318f5ca3c1d814dfefa
Ignacio Mazuecos <mazuecos18@gmail.com>
2016-02-29 22:54:36 +0100

    Version 1602292253

--- app/src/main/java/dam/aventuradebolsillo/MetodosBD.java
+++ app/src/main/java/dam/aventuradebolsillo/MetodosBD.java
@@ -1,8 +1,48 @@
 package dam.aventuradebolsillo;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MetodosBD {
 
-    public void insertarNuevoUsuario(){
+    HttpClient httpclient = new DefaultHttpClient();
+    List<NameValuePair> nameValuePairs;
+    HttpPost httppost;
 
+    public boolean insertarNuevoUsuario(String usuario, String contraseña, String clase){
+        if(clase.equals("Guerrero")){
+            httppost = new HttpPost("AQUÍ DEBEMOS INTRODUCIR LA URL DEL PHP DE 'insertarNuevoGuerrero' QUE ESTÁ EN EL SERVIDOR"); //Ejemplo: "http://192.168.0.11/picarcodigo/insert.php"
+        }else if(clase.equals("Mago")){
+            httppost = new HttpPost("AQUÍ DEBEMOS INTRODUCIR LA URL DEL PHP DE 'insertarNuevoMago' QUE ESTÁ EN EL SERVIDOR"); //Ejemplo: "http://192.168.0.11/picarcodigo/insert.php"
+        }else{
+            httppost = new HttpPost("AQUÍ DEBEMOS INTRODUCIR LA URL DEL PHP DE 'insertarNuevoPicaro' QUE ESTÁ EN EL SERVIDOR"); //Ejemplo: "http://192.168.0.11/picarcodigo/insert.php"
+        }
+        //A partir de aquí comenzamos a recoger los datos
+        nameValuePairs = new ArrayList<NameValuePair>(3);
+        nameValuePairs.add(new BasicNameValuePair("usuario", usuario));
+        nameValuePairs.add(new BasicNameValuePair("contraseña", contraseña));
+
+        try{
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            httpclient.execute(httppost);
+            return true;
+        }catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }catch (ClientProtocolException e) {
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return false;
     }
 }
