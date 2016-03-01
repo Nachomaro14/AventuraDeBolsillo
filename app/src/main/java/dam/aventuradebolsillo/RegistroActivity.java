5c6cf5461eff4d9d46ac0b62284936a9e50be352
Ignacio Mazuecos <mazuecos18@gmail.com>
2016-03-01 14:57:12 +0100

    Version 1603011457

--- app/src/main/java/dam/aventuradebolsillo/RegistroActivity.java
+++ app/src/main/java/dam/aventuradebolsillo/RegistroActivity.java
@@ -1,13 +1,125 @@
 package dam.aventuradebolsillo;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegistroActivity extends Activity{
+
+    ImageButton registrar;
+    CheckBox guerrero, mago, picaro;
+    EditText usuario, contrasena;
+    HttpClient httpclient = new DefaultHttpClient();
+    List<NameValuePair> nameValuePairs;
+    HttpPost httppost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        registrar = (ImageButton) findViewById(R.id.btnNuevoUsuario);
+        guerrero = (CheckBox) findViewById(R.id.checkGuerrero);
+        mago = (CheckBox) findViewById(R.id.checkMago);
+        picaro = (CheckBox) findViewById(R.id.checkPicaro);
+
+        usuario = (EditText) findViewById(R.id.txtNuevoUsuario);
+        contrasena = (EditText) findViewById(R.id.txtNuevaContraseña);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Registrar(RegistroActivity.this).execute();
+            }
+        });
+    }
+
+    public boolean insertarNuevoUsuario(){
+        String u = usuario.getText().toString();
+        String p = contrasena.getText().toString();
+        String c = "";
+        if(guerrero.isChecked()){
+            c = "Guerrero";
+        }else if(mago.isChecked()){
+            c = "Mago";
+        }else{
+            c = "Picaro";
+        }
+        if(!u.equals("") && !p.equals("") && !c.equals("")){
+            if(c.equals("Guerrero")){
+                httppost = new HttpPost("http://aventuradebolsillo.esy.es/insertarNuevoGuerrero.php");
+            }else if(c.equals("Mago")){
+                httppost = new HttpPost("http://aventuradebolsillo.esy.es/insertarNuevoMago.php");
+            }else{
+                httppost = new HttpPost("http://aventuradebolsillo.esy.es/insertarNuevoPicaro.php");
+            }
+
+            nameValuePairs = new ArrayList<NameValuePair>(3);
+            nameValuePairs.add(new BasicNameValuePair("usuario", u));
+            nameValuePairs.add(new BasicNameValuePair("contrasena", p));
+
+            try{
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                httpclient.execute(httppost);
+                return true;
+            }catch(UnsupportedEncodingException e){
+                e.printStackTrace();
+            }catch (ClientProtocolException e) {
+                e.printStackTrace();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public class Registrar extends AsyncTask<String, String, String>{
+
+        private Activity ctx;
+
+        Registrar(Activity ctx){
+            this.ctx = ctx;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(insertarNuevoUsuario()){
+                ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ctx, "Creado un nuevo usuario", Toast.LENGTH_LONG).show();
+                        usuario.setText("");
+                        contrasena.setText("");
+                    }
+                });
+            }else{
+                ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ctx, "No ha sido posible crear el nuevo usuario", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+            return null;
+        }
     }
 }
